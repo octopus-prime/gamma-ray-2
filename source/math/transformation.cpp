707dc238ca5b08452607787cccdc242696097998
octopus-prime <mike_gresens@notebook>
2016-02-03 21:35:45 +0100

    Code clean up.

--- source/math/transformation.cpp
+++ source/math/transformation.cpp
@@ -45,7 +45,7 @@ scaling(const vector_t& vector)
 		{0, 0, 0, 1}
 	}};
 }
-/*
+
 matrix_t
 rotation(const vector_t& vector)
 {
@@ -61,90 +61,13 @@ rotation(const vector_t& vector)
 	const float s = std::sin(angle * factor);
 	const float c = std::cos(angle * factor);
 
-//	matrix_t m
-//	{{
-//		{x * x * (1 - c) + c,		x * y * (1 - c) + s * z,	x * z * (1 - c) - s * y,	0},
-//		{x * y * (1 - c) - s * z,	y * y * (1 - c) + c,		y * z * (1 - c) + s * x,	0},
-//		{x * z * (1 - c) + s * y,	y * z * (1 - c) - s * x,	z * z * (1 - c) + c,		0},
-//		{0,							0,							0,							1}
-//	}};
-
-	matrix_t m
+	return matrix_t
 	{{
-		{x * x + (1 - x * x) * c,	x * y * (1 - c) + s * z,	x * z * (1 - c) - s * y,	0},
-		{x * y * (1 - c) - s * z,	y * y + (1 - y * y) * c,	y * z * (1 - c) + s * x,	0},
-		{x * z * (1 - c) + s * y,	y * z * (1 - c) - s * x,	z * z + (1 - z * z) * c,	0},
+		{x * x * (1 - c) + c,		x * y * (1 - c) + s * z,	x * z * (1 - c) - s * y,	0},
+		{x * y * (1 - c) - s * z,	y * y * (1 - c) + c,		y * z * (1 - c) + s * x,	0},
+		{x * z * (1 - c) + s * y,	y * z * (1 - c) - s * x,	z * z * (1 - c) + c,		0},
 		{0,							0,							0,							1}
 	}};
-
-//	const auto f = [&](const vector_t i)
-//	{
-//		return normalized * dot(normalized, i) + c * cross(cross(normalized, i), normalized) + s * cross(normalized, i);
-//	};
-//
-//	matrix_t n
-//	{{
-//		f(vector_t {1,0,0,0}),
-//		f(vector_t {0,1,0,0}),
-//		f(vector_t {0,0,1,0}),
-//		{0, 0, 0, 1}
-//	}};
-//
-//	std::cout << m << std::endl;
-//	std::cout << n << std::endl;
-
-	return m;
-}
-*/
-
-matrix_t
-rotation(const vector_t& vector)
-{
-	constexpr float f = -M_PI / 180.0;
-
-	const auto rotateX = [&vector]() -> matrix_t
-	{
-		const float angle = f * vector[0];
-		const float s = std::sin(angle);
-		const float c = std::cos(angle);
-		return matrix_t
-		{{
-			{1.0,	0.0,	0.0,	0.0},
-			{0.0,	+c,		+s,		0.0},
-			{0.0,	-s,		+c,		0.0},
-			{0.0,	0.0,	0.0,	1.0}
-		}};
-	};
-
-	const auto rotateY = [&vector]() -> matrix_t
-	{
-		const float angle = f * vector[1];
-		const float s = std::sin(angle);
-		const float c = std::cos(angle);
-		return matrix_t
-		{{
-			{+c,	0.0,	-s,		0.0},
-			{0.0,	1.0,	0.0,	0.0},
-			{+s,	0.0,	+c,		0.0},
-			{0.0,	0.0,	0.0,	1.0}
-		}};
-	};
-
-	const auto rotateZ = [&vector]() -> matrix_t
-	{
-		const float angle = f * vector[2];
-		const float s = std::sin(angle);
-		const float c = std::cos(angle);
-		return matrix_t
-		{{
-			{+c,	+s,		0.0,	0.0},
-			{-s,	+c,		0.0,	0.0},
-			{0.0,	0.0,	1.0,	0.0},
-			{0.0,	0.0,	0.0,	1.0}
-		}};
-	};
-
-	return rotateX() * rotateY() * rotateZ();
 }
 
 }
